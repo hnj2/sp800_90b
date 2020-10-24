f73e7c2dd963ffb31b2d1d453af57091211acf9f
Hans Niklas Jacob <hnj@posteo.de>
2020-10-24 12:22:07 +0200

    Hopefully this works with readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,12 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+#os.system('sudo apt install libbz2-dev libdivsufsort-dev build-essential')
+os.system('cd .. && python setup.py install')
+
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
